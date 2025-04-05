425d6724d92d1dc04eb29d14dc09b024db9a0ab3
emyokellow@gmail.com <emyokellow@gmail.com>
2025-04-05 16:31:11 +0300

    Initial commit of Mindfulness Program - only relevant files

---
+++ week05/Mindfulness/Activity.cs
@@ -0,0 +1,68 @@
+// Activity.cs
+using System;
+
+public class Activity
+{
+    protected string _name;
+    protected string _description;
+    protected int _duration;
+
+    public Activity(string name, string description)
+    {
+        _name = name;
+        _description = description;
+    }
+
+    public void StartMessage()
+    {
+        Console.Clear();
+        Console.WriteLine($"Welcome to the {_name} Activity.");
+        Console.WriteLine();
+        Console.WriteLine(_description);
+        Console.WriteLine();
+        Console.Write("How long, in seconds, would you like for your session? ");
+        _duration = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+        Console.WriteLine("Get ready...");
+        ShowSpinner(3);
+    }
+
+    public void EndMessage()
+    {
+        Console.WriteLine();
+        Console.WriteLine("Well done!!");
+        ShowSpinner(3);
+        Console.WriteLine();
+        Console.WriteLine($"You have completed another {_duration} seconds of the {_name} Activity.");
+        ShowSpinner(3);
+    }
+
+    protected void ShowSpinner(int seconds)
+    {
+        for (int i = 0; i < seconds * 2; i++)
+        {
+            Console.Write("/");
+            Thread.Sleep(250);
+            Console.Write("\b \b");
+            Console.Write("-");
+            Thread.Sleep(250);
+            Console.Write("\b \b");
+            Console.Write("\\");
+            Thread.Sleep(250);
+            Console.Write("\b \b");
+            Console.Write("|");
+            Thread.Sleep(250);
+            Console.Write("\b \b");
+        }
+    }
+
+    protected void ShowCountdown(int seconds)
+    {
+        for (int i = seconds; i > 0; i--)
+        {
+            Console.Write(i);
+            Thread.Sleep(1000);
+            Console.Write("\b \b");
+        }
+    }
+}
